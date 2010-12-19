b588213ccf61a45a1e0e96f626c528483b3aea7e
Simon Sapin <simon.sapin@exyr.org>
2010-12-19 23:07:45 +0900

    Change version number to 0.1dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Static',
-    version='0.1',
+    version='0.1dev',
     url='http://exyr.org/Flask-Static/',
     license='BSD',
     author='Simon Sapin',
