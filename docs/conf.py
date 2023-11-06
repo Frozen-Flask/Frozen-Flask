31722214f1f51997f3f0dcb13a9f217ca55b0d13
Guillaume Ayoub <guillaume@courtbouillon.org>
2023-11-06 14:26:25 +0100

    Clean and improve documentation style

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 from re import search
 
@@ -5,11 +6,15 @@ from re import search
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'pallets_sphinx_themes',
+]
 
 intersphinx_mapping = {
-    'flask': ('https://flask.palletsprojects.com/', None),
     'python': ('https://docs.python.org/3/', None),
+    'flask': ('https://flask.palletsprojects.com/', None),
     'click': ('https://click.palletsprojects.com/', None),
 }
 
@@ -24,7 +29,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Frozen-Flask'
-copyright = '2010-2023, Simon Sapin'
+copyright = f'2010-{datetime.now().year}, Simon Sapin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -51,13 +56,12 @@ toc_object_entries = False
 # a list of builtin themes.
 html_theme = 'flask'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    'index_logo': 'artwork/frozen-flask.png',
-    'index_logo_height': '156px',
-}
+# A list of CSS files. The entry must be a filename string or a tuple
+# containing the filename string and the attributes dictionary. The filename
+# must be relative to the html_static_path, or a full URI with scheme like
+# https://example.org/style.css. The attributes is used for attributes of
+# <link> tag.
+html_css_files = ['style.css']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
